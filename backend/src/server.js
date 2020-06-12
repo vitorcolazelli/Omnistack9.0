9e48e8b4078add26c64066083eaaa8958e534240
Vitorgpc <vitorcolazelli@gmail.com>
2020-06-12 14:18:51 -0300

    Projeto AirCnC

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9.xpczi.mongodb.net/Omnistack9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
