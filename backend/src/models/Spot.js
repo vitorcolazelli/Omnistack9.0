9983edd4a95226bf2f669f0e3d03b51163a149c1
Vitorgpc <vitorcolazelli@gmail.com>
2020-06-12 16:27:01 -0300

    Atualizacao socket

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -16,7 +16,7 @@ const SpotSchema = new mongoose.Schema({
 });
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://192.168.0.30:3333/files/${this.thumbnail}`
+    return `http://192.168.0.32:3333/files/${this.thumbnail}`
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,32 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9.xpczi.mongodb.net/Omnistack9?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -16,4 +36,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
